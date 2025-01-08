e9899879fa7b3523c89379262ede8eac88dc41ed
Dr_Delin <38278447+DrDelin@users.noreply.github.com>
2025-01-08 15:33:35 +0530

    Removal scripts are removed

--- install.sh
+++ install.sh
@@ -50,12 +50,6 @@ else
     pip install gdown
     apt install rclone -y
 
-    #Removed Features
-    pip3 uninstall termux-apt-repo -y
-    pkg remove x11-repo -y
-    pkg remove wget -y
-    apt remove transmission-gtk -y
-
     #Updates and upgrades:
     apt update -y -y -y
     apt upgrade -y -y -y
